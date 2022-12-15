705f2f57ecbcae78a0d3514954a1510ef5db240a
nzr_pe <joseluis.von@gmail.com>
2022-12-14 21:42:03 -0500

    ultimopartido

--- app/Http/Controllers/PartidoController.php
+++ app/Http/Controllers/PartidoController.php
@@ -45,7 +45,7 @@ class PartidoController extends Controller
         //
         $input = $request->all();
         Partido::create($input);
-        $ultimopartido = Partido::first();
+        $ultimopartido = Partido::orderBy('id', 'ASC')->first();
         return Response::json(array(
             'success'=>true,
             'mensaje' => 'Se Guardaron los datos',
